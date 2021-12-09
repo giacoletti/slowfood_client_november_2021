468c022fa780187f741f487f4144d68f4e151bb2
Elvita Kalvāne <elvita.kalvane@gmail.com>
2021-12-09 15:23:08 +0100

    complete e2e specs for user can see products and implement functionality

--- cypress/integration/userCanSeeListOfProducts.feature.js
+++ cypress/integration/userCanSeeListOfProducts.feature.js
@@ -13,22 +13,18 @@ describe("A user that visits the application", () => {
   });
 
   it("is expected to see a collection of product", () => {
-    cy.get("[data-cy=product-list]").children().should("have.length", 2);
+    cy.get("[data-cy=product-list]").children().should("have.length", 3);
   });
 
   it("is expected to see products with a name ", () => {
-    cy.get("[data-cy=product-list]").first().should("contain", "pizza");
+    cy.get("[data-cy=product-list]").first().should("contain", "Pizza");
   });
 
-  // it('is expected can see products with a price ', () => {
-  //     cy.get('[data-cy=product-list]').price. should eq 100
-  // })
-  // it('is expected can see products with name, category and a price ', () => {
-  //     cy.get('[data-cy=product-list]').should("contain", "maincourse")
-  // })
+  it("is expected can see products with a price ", () => {
+    cy.get("[data-cy=product-list]").first().should("contain", 200);
+  });
 
-  //  it('is expected to have a button to add product to the order ', () => {
-  //      cy.get('[data-cy=button_select]').should("exist")
-
-  //  })
+  it("is expected to have a button to add product to the order ", () => {
+    cy.get("[data-cy=button_select]").first().should("contain", "Add to Order");
+  });
 });
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import Products from "./modules/Products";
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    Products.index().then((data) => {
+      setProducts(data);
+    });
+  }, []);
+
+  const productsList = products.map((product) => {
+    return (
+      <div key={products.id}>
+        {product.name} - {`${product.price}`}{" "}
+        <button data-cy="button_select">Add to Order</button>
+      </div>
+    );
+  });
+
   return (
     <>
       <h1>Slowfood</h1>
-      
+      <div data-cy="product-list">{productsList}</div>
     </>
   );
-}
+};
 
 export default App;

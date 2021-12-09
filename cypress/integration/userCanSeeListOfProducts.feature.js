83feb450565d6d63c4a6d1db4c06b0fff850ff06
Elvita Kalvāne <elvita.kalvane@gmail.com>
2021-12-09 13:21:40 +0100

    add axios and WIP for spec implementation

--- cypress/integration/userCanSeeListOfProducts.feature.js
+++ cypress/integration/userCanSeeListOfProducts.feature.js
@@ -1,37 +1,34 @@
+/* eslint-disable no-undef */
 
-describe ('A user that visits the application', () => {
+describe("A user that visits the application", () => {
+  before(() => {
+    cy.intercept("GET", "**/api/products", { fixture: "products.json" }).as(
+      "Products.index"
+    );
+    cy.visit("/");
+  });
 
-    before(() => {
-        cy.intercept(
-            "GET", 
-            "**/api/products",
-            {fixtures: "products.json"}
-            ).as("getProducts")
-        cy.visit("/");
-    });
+  it("is expected to make a network call with a status 200", () => {
+    cy.wait("@Products.index").its("response.statusCode").should("eq", 200);
+  });
 
-    it('is expected to make a network call with a status 200', () => {
-        cy.wait("@getProducts").its('response.statusCode').should('eq', 200);
-    })
+  it("is expected to see a collection of product", () => {
+    cy.get("[data-cy=product-list]").children().should("have.length", 2);
+  });
 
-    it('is expected to see a collection of product', () => {
-        cy.get('[data-cy=product-list]').children().should('have.length', 2)
-    })
+  it("is expected to see products with a name ", () => {
+    cy.get("[data-cy=product-list]").first().should("contain", "pizza");
+  });
 
-    it('is expected to see products with a name ', () => {
-        cy.get('[data-cy=product-list]').first().should("contain", "pizza")
-    })
+  // it('is expected can see products with a price ', () => {
+  //     cy.get('[data-cy=product-list]').price. should eq 100
+  // })
+  // it('is expected can see products with name, category and a price ', () => {
+  //     cy.get('[data-cy=product-list]').should("contain", "maincourse")
+  // })
 
-    // it('is expected can see products with a price ', () => {
-    //     cy.get('[data-cy=product-list]').price. should eq 100
-    // })
-    // it('is expected can see products with name, category and a price ', () => {
-    //     cy.get('[data-cy=product-list]').should("contain", "maincourse")
-    // })
+  //  it('is expected to have a button to add product to the order ', () => {
+  //      cy.get('[data-cy=button_select]').should("exist")
 
-    //  it('is expected to have a button to add product to the order ', () => {
-    //      cy.get('[data-cy=button_select]').should("exist")
-
-    //  })
-
-})
+  //  })
+});

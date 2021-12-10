56c6e87657957e3e0dbc85c466638ae0ca6f1dd7
DefyCab <love.bellinder@gmail.com>
2021-12-10 16:09:39 +0100

    removed debugger and premature fixture

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,40 @@
-import React, { useState, useEffect } from "react";
-import Products from "./modules/Products";
-import Orders from "./modules/Orders";
+import React, { useState, useEffect } from 'react'
+import Products from './modules/Products'
+import Orders from './modules/Orders'
 
 const App = () => {
-  const [products, setProducts] = useState([]);
-  const [order, setOrder] = useState();
+  const [products, setProducts] = useState([])
+  const [order, setOrder] = useState()
 
   useEffect(() => {
     Products.index().then((data) => {
-      setProducts(data);
-    });
-  }, []);
+      setProducts(data)
+    })
+  }, [])
 
   const addToOrder = (product_id) => {
     Orders.create(product_id, 1).then((response) => {
-      debugger;
-      setOrder(response.order);
-    });
-  };
+      setOrder(response.order)
+    })
+  }
 
   const productsList = products.map((product) => {
     return (
       <div key={products.id}>
-        {product.name} - {`${product.price}`}{" "}
+        {product.name} - {`${product.price}`}{' '}
         <button data-cy="button_select" onClick={() => addToOrder(product.id)}>
           Add to Order
         </button>
       </div>
-    );
-  });
+    )
+  })
 
   return (
     <>
       <h1>Slowfood</h1>
       <div data-cy="product-list">{productsList}</div>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App

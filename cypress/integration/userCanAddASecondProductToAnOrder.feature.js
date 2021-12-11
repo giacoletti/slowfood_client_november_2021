07c87867b80ab85509b36b8a111125350e9c95a7
giacoletti <giacoletti92@gmail.com>
2021-12-11 15:36:39 +0100

    Adds end2end test to check if user is able to add a second product to order and make a put request creates orderUpdateResponse fixture

---
+++ cypress/integration/userCanAddASecondProductToAnOrder.feature.js
@@ -0,0 +1,29 @@
+describe('A user clicking on "Add to Order" button for a second product', () => {
+  before(() => {
+    cy.intercept("GET", "**/api/products", {
+      fixture: "productsIndexResponse.json",
+    });
+    cy.intercept("POST", "**/api/orders", {
+      fixture: "orderCreateResponse.json",
+    });
+    cy.intercept("PUT", "**/api/orders/**", {
+      fixture: "orderUpdateResponse.json",
+    }).as("Orders.update");
+    cy.visit('/');
+
+    cy.get("[data-cy=product-list]").children().first().within(() => {
+      cy.get("[data-cy=add-to-order-button]").click();
+    });
+    cy.get("[data-cy=product-list]").children().last().within(() => {
+      cy.get("[data-cy=add-to-order-button]").click();
+    });
+  });
+
+  it('is expected to make a PUT request to the API', () => {
+    cy.wait('@Orders.update').its('request.method').should('eq', 'PUT');
+  });
+
+  it('is expected to render message', () => {
+    cy.get('[data-cy=message-box]').should('contain.text', 'Risotto was added to your order!')
+  });
+});

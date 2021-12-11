122ce6cda234641d96c5c90420e19ab21c95a531
giacoletti <giacoletti92@gmail.com>
2021-12-11 18:02:34 +0100

    Adds end2end test to check if user can display order details

---
+++ cypress/integration/userCanSeeOrderDetails.feature.js
@@ -0,0 +1,29 @@
+describe('A user clicking on "View Order"', () => {
+
+  before(() => {
+    cy.intercept("GET", "**/api/products", {
+      fixture: "productsIndexResponse.json",
+    });
+    cy.intercept("POST", "**/api/orders", {
+      fixture: "orderCreateResponse.json",
+    });
+    cy.intercept("PUT", "**/api/orders/**", {
+      fixture: "orderUpdateResponse.json",
+    });
+    cy.visit('/');
+
+    cy.get("[data-cy=product-list]").children().first().within(() => {
+      cy.get("[data-cy=add-to-order-button]").click();
+    });
+    cy.get("[data-cy=product-list]").children().last().within(() => {
+      cy.get("[data-cy=add-to-order-button]").click();
+    });
+
+    cy.get('[data-cy=view-order]').click();
+  });
+
+  it('is expected to display order details and hide product list', () => {
+    cy.get('[data-cy=product-list]').should('not.exist');
+    cy.get('[data-cy=order-details]').should('be.visible');
+  });
+});
